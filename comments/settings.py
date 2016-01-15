3fee11233836490d9ca1434888f6b44dfc2dd4d5
Andrew M. Farrell <amfarrell@mit.edu>
2016-01-15 07:40:50 +0000

    Fall back to sqlite if there is no db url.

--- comments/settings.py
+++ comments/settings.py
@@ -87,9 +87,18 @@ WSGI_APPLICATION = 'comments.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(),
-}
+if os.environ('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(),
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
